06e6775b75973b463fee122854a721371d923299
Mohamed Hazem <darkg5341@gmail.com>
2026-04-05 00:57:04 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+
+let menuBtn = document.querySelector(".menuToggle")
+let smallMenu = document.querySelector(".smallMenu")
+
+menuBtn.addEventListener("click", ()=>{
+    smallMenu.classList.toggle("active")
+})
+
+
+
+document.addEventListener("click", (e) => {
+  if (!smallMenu.contains(e.target) && !menuBtn.contains(e.target)) {
+        smallMenu.classList.add("close");
+        setTimeout(() => {
+            smallMenu.classList.remove("close");
+            smallMenu.classList.remove("active");
+        }, 400);
+  }
+});
+console.log(menuBtn)
