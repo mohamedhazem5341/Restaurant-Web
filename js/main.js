072e6b187d73662e7e87a4dd9ce3293de61708fe
Mohamed Hazem <darkg5341@gmail.com>
2026-04-10 11:09:51 +0200

    slide functionality

--- js/main.js
+++ js/main.js
@@ -1,21 +1,63 @@
+let menuBtn = document.querySelector(".menuToggle");
+let smallMenu = document.querySelector(".smallMenu");
 
-
-let menuBtn = document.querySelector(".menuToggle")
-let smallMenu = document.querySelector(".smallMenu")
-
-menuBtn.addEventListener("click", ()=>{
-    smallMenu.classList.toggle("active")
-})
-
-
+menuBtn.addEventListener("click", () => {
+  smallMenu.classList.toggle("active");
+});
 
 document.addEventListener("click", (e) => {
   if (!smallMenu.contains(e.target) && !menuBtn.contains(e.target)) {
-        smallMenu.classList.add("close");
-        setTimeout(() => {
-            smallMenu.classList.remove("close");
-            smallMenu.classList.remove("active");
-        }, 400);
+    smallMenu.classList.add("close");
+    setTimeout(() => {
+      smallMenu.classList.remove("close");
+      smallMenu.classList.remove("active");
+    }, 400);
   }
 });
-console.log(menuBtn)
+
+// slider functionality
+let nextBtn = document.getElementById("next");
+let prevBtn = document.getElementById("prev");
+let slider = document.querySelector(".slides");
+let feeds = document.querySelectorAll(".feeds");
+
+let index = 0;
+
+nextBtn.onclick = () => {
+  if (index < feeds.length - 1) {
+    index++;
+    updateSlider();
+  }
+};
+
+prevBtn.onclick = () => {
+  if (index > 0) {
+    index--;
+    updateSlider();
+  }
+};
+
+// to show as amount of circles as sliders
+
+let pointFeed = document.querySelector(".pointFeed");
+
+for (i = 0; i < feeds.length; i++) {
+  pointFeed.innerHTML += `<div class="circle"></div>`;
+}
+
+let circle = document.querySelectorAll(".circle");
+
+function updateSlider() {
+  slider.style.transform = `translateX(-${index * 103.8}%)`;
+  // to highlight circle number that matches the slide
+  for (i = 0; i < circle.length; i++) {
+    if (index === i) {
+      circle[i].classList.add("active");
+    } else {
+      circle[i].classList.remove("active");
+    }
+  }
+
+  
+}
+updateSlider();
