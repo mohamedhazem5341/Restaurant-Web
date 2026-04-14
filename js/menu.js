1f4177709a85c6917e30bf65177b28e30062bfc1
Mohamed Hazem <darkg5341@gmail.com>
2026-04-15 01:08:12 +0200

    food cards update

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,16 @@
-let dishesInput = document.getElementById("dishesInput");
+let categories = document.querySelector(".categories span");
+let dishes = document.querySelector(".dishes span");
+
+//
+//
 let dishesList = document.querySelectorAll(".dishesList li");
 let foodCard = document.querySelectorAll(".foodMenu .foodCard");
-console.log(foodCard[0].dataset);
-console.log(foodCard[0].dataset.target === dishesList[1].dataset.target);
-console.log(dishesList[1].dataset.target);
 
+// this categories & dishes amount (count) that shows in hero section
+categories.innerHTML = dishesList.length - 1;
+dishes.innerHTML = foodCard.length;
+//
+//
 dishesList.forEach((li, i) => {
   li.addEventListener("click", () => {
     // to add class style for ui changes if active
@@ -12,7 +18,7 @@ dishesList.forEach((li, i) => {
       dishesList[j].classList.remove("selected");
     }
     dishesList[i].classList.add("selected");
-    
+
     // to show only the card user clicked on
     for (k = 0; k < foodCard.length; k++) {
       foodCard[k].classList.add("hide");
@@ -28,3 +34,42 @@ dishesList.forEach((li, i) => {
     }
   });
 });
+//////////////////////
+
+let dishesInput = document.getElementById("dishesInput");
+
+const cardName = Array.from(document.querySelectorAll(".cardInfo .cardName"));
+  console.log(cardName[0].innerHTML);
+  let foodNames = []
+  for(i = 0; i < cardName.length; i++){
+    foodNames.push(cardName[i].innerHTML)
+  }
+  console.log(foodNames);
+
+
+
+
+
+let foodMenu = document.querySelector(".foodMenu");
+
+dishesInput.addEventListener("input", () => {
+  const searchFood = dishesInput.value.toLowerCase();
+  console.log(searchFood);
+
+  //  const result = cardName.filter((name) => name.includes(searchFood));
+  //  console.log(result);
+  
+  // for (i = 0; i < cardName.length; i++) {
+  //   if (cardName[i].innerHTML.includes(searchFood)) {
+  //     foodCard[i].classList.add("show")
+  //   } 
+  //   if (cardName[i].innerHTML.includes(!searchFood)) {
+  //     foodCard[i].classList.add("hide")
+  //   }
+  //   console.log(cardName[i].innerHTML.includes(searchFood));
+  // }
+});
+
+// cardName.filter(name => name.toLowerCase().includes(searchFood))
+
+console.log(cardName.length);
